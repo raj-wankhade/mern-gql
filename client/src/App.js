a7d9828aed297d143a6be7ecf17ddc795628e601
raj-wankhade <rajwankhade789@gmail.com>
2023-07-02 22:07:51 +0530

    apolloclient set up and use useQuery for posts

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,52 @@
 import "./App.css";
 
+import { useQuery, gql } from "@apollo/client";
+
+const GET_POSTS = gql`
+  query {
+    posts {
+      author {
+        firstName
+        id
+      }
+      title
+      id
+    }
+  }
+`;
+
+function DisplayPosts() {
+  const { loading, error, data } = useQuery(GET_POSTS);
+
+  if (error) return <p>{error}</p>;
+
+  if (loading) return <p>Loading ...</p>;
+
+  return (
+    data &&
+    data.posts.map((post) => {
+      return (
+        <div key={post.id}>
+          <h3>{post.id}</h3>
+          <p>Title: {post.title}</p>
+          <br />
+          <b>About this author:</b>
+          {post.author && post.author.id} :{" "}
+          {post.author && post.author.firstName}
+          <br />
+        </div>
+      );
+    })
+  );
+}
+
 function App() {
-  return <div className="App">Helllooo</div>;
+  return (
+    <div className="App">
+      Helllooo
+      <DisplayPosts />
+    </div>
+  );
 }
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -3,9 +3,38 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  gql,
+} from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: process.env.REACT_APP_GRAPHQL_ENDPOINT,
+  cache: new InMemoryCache(),
+});
+
+// run below to confirm the set up
+
+// client
+//   .query({
+//     query: gql`
+//       query {
+//         posts {
+//           authorId {
+//             firstName
+//           }
+//           id
+//         }
+//       }
+//     `,
+//   })
+//   .then((result) => console.log(result));
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>
+  </ApolloProvider>
 );
