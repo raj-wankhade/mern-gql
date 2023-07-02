94dbcebbabd1b77460ecf7a6b75ba73356923dd0
raj-wankhade <rajwankhade789@gmail.com>
2023-07-02 22:59:41 +0530

    useLazyQuery and setting the data to the page with bootstrap

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 
-import { useQuery, gql } from "@apollo/client";
+import { useQuery, gql, useLazyQuery } from "@apollo/client";
 
 const GET_POSTS = gql`
   query {
@@ -17,33 +17,55 @@ const GET_POSTS = gql`
 
 function DisplayPosts() {
   const { loading, error, data } = useQuery(GET_POSTS);
+  const [fetchPosts, { data: posts }] = useLazyQuery(GET_POSTS);
 
   if (error) return <p>{error}</p>;
 
   if (loading) return <p>Loading ...</p>;
 
   return (
-    data &&
-    data.posts.map((post) => {
-      return (
-        <div key={post.id}>
-          <h3>{post.id}</h3>
-          <p>Title: {post.title}</p>
-          <br />
-          <b>About this author:</b>
-          {post.author && post.author.id} :{" "}
-          {post.author && post.author.firstName}
-          <br />
-        </div>
-      );
-    })
+    <div className="container">
+      <div className="row mt-3">
+        {data &&
+          data.posts.map((post, index) => {
+            let odd = null;
+            if (index / 2 === 0) odd = "mb-3 mb-sm-0";
+            return (
+              <div className={`col-sm-6 mb-3 ${index + 1} `} key={post.id}>
+                <div className="card">
+                  <div className="card-body">
+                    <h5 className="card-title">
+                      {post.id} &nbsp;
+                      {post.title}
+                    </h5>
+                    <p className="card-text">
+                      {post.author && post.author.id} is the ID and the author
+                      is &nbsp; {post.author && post.author.firstName}
+                    </p>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+
+        <a
+          href="#"
+          className="btn btn-primary"
+          onClick={() => {
+            fetchPosts();
+          }}
+        >
+          Fetch Posts
+        </a>
+        {JSON.stringify(posts)}
+      </div>
+    </div>
   );
 }
 
 function App() {
   return (
     <div className="App">
-      Helllooo
       <DisplayPosts />
     </div>
   );
--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  gql,
-} from "@apollo/client";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
 const client = new ApolloClient({
   uri: process.env.REACT_APP_GRAPHQL_ENDPOINT,
