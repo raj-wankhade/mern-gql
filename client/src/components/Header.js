13ba06a8bf006f7a4d3897e1b471635669a859af
raj-wankhade <rajwankhade789@gmail.com>
2023-07-16 09:11:20 +0530

    create userSchema as User model

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -28,7 +28,7 @@ export default function Header() {
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
       <div className="container-fluid">
         <Link className="navbar-brand" to="/">
-          MERN-GQL
+          Home
         </Link>
         <button
           className="navbar-toggler"
---
+++ server/models/User.js
@@ -0,0 +1,22 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    username: { type: String, index: true, required: true },
+    email: { type: String, index: true, required: true, unique: true },
+    password: { type: String, required: true },
+    images: {
+      type: Array,
+      default: {
+        url: "https://placehold.co/200x200/000000/FFF?text=profile",
+        public_id: Date.now,
+      },
+    },
+    name: String,
+    about: String,
+    date: { type: Date, default: Date.now },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("User", userSchema);
